d6d77c3775c519e9d99e30f1e8c6a490212d5385
danilabohanov123 <danilabohanov123@yandex.ru>
2020-11-11 20:41:49 +0300

    Модуляция

---
+++ js/announcements-list.js
@@ -0,0 +1,102 @@
+"use strict";
+
+(() => {
+  const shuffleList = (itemsList) => {
+    const usedIndexes = [];
+    for (let i = 0; i < itemsList.length; i++) {
+      usedIndexes.push(false);
+    }
+    const shuffledItemsList = [];
+    for (let i = 0; i < itemsList.length; i++) {
+      const nextIndex = window.util.generateRandomInteger(1, itemsList.length - i);
+      let falseCounter = 0;
+      for (let j = 0; j < usedIndexes.length; j++) {
+        if (!usedIndexes[j]) {
+          falseCounter++;
+          if (falseCounter === nextIndex) {
+            usedIndexes[j] = true;
+            shuffledItemsList.push(itemsList[j]);
+          }
+        }
+      }
+    }
+    return shuffledItemsList;
+  };
+
+  const listIndexes = [];
+  for (let i = 1; i <= window.data.AD_NUMBER; i++) {
+    listIndexes.push(i);
+  }
+
+  const listAvatarIndexes = shuffleList(listIndexes);
+
+  const generateOffer = (title, address, price, type, rooms, guests, checkin, checkout, features,
+      description, photos) => {
+    return {title: title, address: address, price: price, type: type, rooms: rooms, guests: guests,
+      checkin: checkin, checkout: checkout, features: features, description: description, photos: photos};
+  };
+
+  const generateTypes = () => {
+    const typeInd = window.util.generateRandomInteger(0, window.data.hotelTypes.length - 1);
+    return window.data.hotelTypes[typeInd];
+  };
+
+  const generateAccommodationOfGuests = () => {
+    let roomsNumber = window.util.generateRandomInteger(1, 4);
+    let guestsNumber = window.util.generateRandomInteger(roomsNumber, 3);
+    if (roomsNumber === 4) {
+      roomsNumber = 100;
+      guestsNumber = 0;
+    }
+    return {rooms: roomsNumber, guests: guestsNumber};
+  };
+
+  const generateCheckin = () => {
+    return window.data.hotelTimes[window.util.generateRandomInteger(0, window.data.hotelTimes.length - 1)];
+  };
+
+  const generateCheckout = () => {
+    return window.data.hotelTimes[window.util.generateRandomInteger(0, window.data.hotelTimes.length - 1)];
+  };
+
+  const generateRandomSubarray = (arrayItems) => {
+    const randomSubarraySize = window.util.generateRandomInteger(1, arrayItems.length);
+    const randomSubarray = shuffleList(arrayItems).slice(0, randomSubarraySize);
+    return randomSubarray;
+  };
+
+  const generateFeatures = () => {
+    return generateRandomSubarray(window.data.hotelFeatures);
+  };
+
+  const generatePhotos = () => {
+    const RandomSubarrayPhotos = generateRandomSubarray(window.data.hotelPhotos);
+    for (let i = 0; i < RandomSubarrayPhotos.length; i++) {
+      RandomSubarrayPhotos[i] = `http://o0.github.io/assets/images/tokyo/hotel` + RandomSubarrayPhotos[i] + `.jpg`;
+    }
+    return RandomSubarrayPhotos;
+  };
+
+  const generateLocation = () => {
+    return {x: window.util.generateRandomInteger(0, 100), y: window.util.generateRandomInteger(130, 630)};
+  };
+
+  const generateAnnouncement = (ind) => {
+    const author = {avatar: `img/avatars/user0` + listAvatarIndexes[ind] + `.png`};
+    const location = generateLocation();
+    const livingSetup = generateAccommodationOfGuests();
+    const offer = generateOffer(`Title`, location.x + `, ` + location.y,
+        window.util.generateRandomInteger(10000, 1000000), generateTypes(), livingSetup.rooms,
+        livingSetup.guests, generateCheckin(), generateCheckout(),
+        generateFeatures(), `Some words about hotel.`, generatePhotos());
+    return {author: author, offer: offer, location: location};
+  };
+
+  const announcementsList = [];
+
+  for (let i = 0; i < window.data.AD_NUMBER; i++) {
+    announcementsList.push(generateAnnouncement(i));
+  }
+
+  window.announcementsList = announcementsList;
+})();
